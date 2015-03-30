59b3fb0a0148fddbe014c5c25b26b97151c34f40
Moaead <moaead@foothillsolutions.com>
2015-03-31 00:21:52 +0300

    Kata TDD

---
+++ UnitTestProject1/Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    class Calculator
+    {
+        public int Add(string input)
+        {
+            var negatives = new List<int>();
+            if (input.StartsWith("//"))
+            {
+                if (input.StartsWith("//["))
+                {
+                    List<String> delems = new List<String>();
+                    int pos1 = input.IndexOf("[");
+                    while (pos1 >= 2)
+                    {
+
+                        int pos2 = input.IndexOf("]", pos1);
+                        String delem = input.Substring(pos1 + 1, pos2 - pos1);
+                        delems.Add(delem);
+                        pos1 = input.IndexOf("[", pos1 + 1);
+                    }
+
+                    input = input.Substring(input.IndexOf("\n") + 1);
+                    foreach (String delem in delems)
+                    {
+
+                        input = input.Replace(delem.Replace("]", ""), ",");
+                    }
+                }
+                else
+                {
+                    int pos1 = 2;
+                    int newLinePos = input.IndexOf("\n");
+                    String delim = input.Substring(pos1, newLinePos - pos1);
+                    input = input.Substring(newLinePos + 1).Replace(delim, ",");
+                }
+
+            }
+
+
+
+            input = input.Replace("\n", ",");
+
+            var split = input.Split(',');
+            int result = 0;
+            if (input.Contains(','))
+                foreach (var a in split)
+                {
+                    int num = int.Parse(a);
+                    if (num >= 1000)
+                        num %= 1000;
+                    result += num;
+                    if (num < 0)
+                        negatives.Add(num);
+
+                }
+            else
+            {
+                int.TryParse(input, out result);
+                if (result < 0)
+                {
+                    throw new Exception("negatives not allowed - " + result);
+                }
+                else if (result >= 1000)
+                    result %= 1000;
+
+
+
+            }
+            if (negatives.Count() > 0)
+            {
+
+                throw new Exception("negatives not allowed - " + String.Join(",", negatives.ToArray()));
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculator calc = new Calculator();
+
+        [TestMethod]
+        public void Given0NumbersReturn0()
+        {
+            var input = "";
+            int result = calc.Add(input);
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Given1NumbersReturnNumber()
+        {
+            String input = "1";
+            int result = calc.Add(input);
+            Assert.AreEqual(result, 1);
+
+        }
+       
+        [TestMethod]
+        public void Given2NumbersReturnSum()
+        {
+            String input = "1,2";
+            
+            int result = calc.Add(input);
+            
+            Assert.AreEqual(result, 3);
+
+        }
+        [TestMethod]
+        public void GivenAnyAmountOfnumbersReturnSum()
+        {
+            String input = "1,2,5,6,7,8,2";
+
+            int result = calc.Add(input);
+
+            Assert.AreEqual(result, 1+2+5+6+7+8+2);
+        }
+        
+        [TestMethod]
+        public void GivenNumbersWithNewLine()
+        {
+            String input = "1\n2,3";
+            int result = calc.Add(input);
+            Assert.AreEqual(result, 6);
+        }
+        [TestMethod]
+        public void GivenNumbersWithDilmReturnSum()
+        {
+            String input = "//;\n1;2";
+            int result = calc.Add(input);
+            Assert.AreEqual(result, 3);
+        }
+        [TestMethod]
+        public void GivenNegativeNumberReturnTheNumber()
+        {
+            try
+            {
+                String input = "-1";
+                int result = calc.Add(input);
+                Assert.IsTrue(false);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.Message.StartsWith("negatives"));
+            }
+            
+        }
+        [TestMethod]
+        public void GivenNegativeNumbersReturnThem()
+        {
+            try
+            {
+                String input = "-1,-2,-3,-4,5,6";
+                int result = calc.Add(input);
+                Assert.IsTrue(false);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.Message.StartsWith("negatives") && e.Message.Contains("-4"));
+            }
+            
+        }
+        [TestMethod]
+        public void GivenNumbersLargenThan1000IgnoreThem()
+        {
+            String input = "1002, 1, 2";
+            int result = calc.Add(input);
+            Assert.AreEqual(result, 5);
+        }
+        [TestMethod]
+        public void GivenNumbersWithMultipleStringDilmReturnSum()
+        {
+            String input = "//[***]\n1***2***3";
+            int result = calc.Add(input);
+            Assert.AreEqual(result, 6);
+        }
+        [TestMethod]
+        public void GivenNumbersWithMultipleDilmReturnSum()
+        {
+            String input = "//[*][%]\n1*2%3";
+            int result = calc.Add(input);
+            Assert.AreEqual(result, 6);
+        }
+        
+        
+
+    }
+}
